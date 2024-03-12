8cadf0c892addf12913a0bdd552173bc59ed06f9
carloslopllo <carlos.lopez.llorente@alumnos.upm.es>
2024-03-12 19:04:24 +0100

    context & project_name

--- Arquetipo1/v2/scripts/central_node_pkg/node.py
+++ Arquetipo1/v2/scripts/central_node_pkg/node.py
@@ -1,13 +1,14 @@
 import os
+import re
 import sys
 import yaml
 from jinja2 import Environment, FileSystemLoader
 
-# sys.argv[1] = /home/vboxuser/ros2_ws/manifest/central_node_pkg/manifest.yaml
-# sys.argv[2] = /home/vboxuser/ros2_ws/templates/central_node_pkg/
+# sys.argv[1] = /home/vboxuser/ros2_ws/manifest/node/manifest.yaml
+# sys.argv[2] = /home/vboxuser/ros2_ws/templates/node/
 
 if len(sys.argv) != 3:
-	print("Usage:", sys.argv[0], "/ros2_ws/manifest/central_node_pkg/manifest.yaml", "/ros2_ws/templates/central_node_pkg/")
+	print("Usage:", sys.argv[0], "~/ros2_ws/manifest/node/manifest.yaml", "~/ros2_ws/templates/node/")
 	sys.exit(1)
 
 with open(sys.argv[1], "r") as f:
@@ -23,35 +24,12 @@ for section in ("buildtools", "dependencies", "testing_CMakeLists"):
 		for item in yp[section]:
 			item.setdefault("type", default_type)
 
-context = {
-	# package
-	"project_name": yp["project_name"],
-	"project_version": yp["project_version"],
-	"project_description": yp["project_description"],
-	"maintainer_email": yp["maintainer_email"],
-	"maintainer": yp["maintainer"],
-	"license": yp["license"],
-
-	# dependencies
-	"buildtools": yp["buildtools"],
-	"dependencies": yp["dependencies"],
-	"testing_CMakeLists": yp["testing_CMakeLists"],
-	"testing_package": yp["testing_package"],
-
-	# class config
-	"class_name": yp["class_name"],
-	"includes": yp["includes"],
-	"inputs": yp["inputs"],
-	"outputs": yp["outputs"],
-}
-
 for path, _, files in os.walk(sys.argv[2]):
 	for file in files:
-		path_file = os.path.join(path, file)
 		environment = Environment(loader=FileSystemLoader(path))
-
 		path_file_template = environment.get_template(file)
-		new_path_file = path_file.replace("templates", "src")
+
+		new_path_file = re.sub("/templates/[^/]*?/", "/src/{project_name}/".format(**yp), os.path.join(path, file))
 		new_path_file = new_path_file.replace(".jinja2", "")
 
 		try:
@@ -60,5 +38,5 @@ for path, _, files in os.walk(sys.argv[2]):
 			os.makedirs(os.path.dirname(new_path_file))
 		finally:
 			with open(new_path_file, mode="w", encoding="utf-8") as results:
-				results.write(path_file_template.render(context))
+				results.write(path_file_template.render(yp))
 				print(f"... wrote {new_path_file}")
